73233365ddecef3ae822f819ff0fda277de627ae
David Wallace Croft <david@CroftSoft.com>
2025-12-02 19:38:12 -0600

    dragon

--- bevy-hello/src/main.rs
+++ bevy-hello/src/main.rs
@@ -1,5 +1,24 @@
-use bevy::prelude::*;
+use self::dragon::Dragon;
+use ::bevy::prelude::*;
+
+mod dragon;
 
 fn main() {
-  App::new().add_plugins(DefaultPlugins).run();
+  App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, setup)
+    .run();
+}
+
+fn setup(
+  mut commands: Commands,
+  asset_server: Res<AssetServer>,
+) {
+  commands.spawn(Camera2d::default());
+
+  let dragon_image: Handle<Image> = asset_server.load("dragon.png");
+
+  commands
+    .spawn(Sprite::from_image(dragon_image))
+    .insert(Dragon);
 }
