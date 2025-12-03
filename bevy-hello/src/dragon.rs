bb1cd7550f6bb84480345c8b9fc8742b659081f4
David Wallace Croft <david@CroftSoft.com>
2025-12-03 17:56:01 -0600

    movement

--- bevy-hello/src/dragon.rs
+++ bevy-hello/src/dragon.rs
@@ -2,3 +2,38 @@ use ::bevy::prelude::*;
 
 #[derive(Component)]
 pub struct Dragon;
+
+impl Dragon {
+  pub fn movement(
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut dragon_query: Query<&mut Transform, With<Dragon>>,
+  ) {
+    let mut x: f32 = 0.;
+
+    let mut y: f32 = 0.;
+
+    if keyboard.pressed(KeyCode::ArrowLeft) {
+      x -= 1.;
+    }
+
+    if keyboard.pressed(KeyCode::ArrowRight) {
+      x += 1.;
+    }
+
+    if keyboard.pressed(KeyCode::ArrowDown) {
+      y -= 1.;
+    }
+
+    if keyboard.pressed(KeyCode::ArrowUp) {
+      y += 1.;
+    }
+
+    dragon_query.iter_mut().for_each(|mut transform| {
+      transform.translation += Vec3 {
+        x,
+        y,
+        z: 0.,
+      };
+    })
+  }
+}
--- bevy-hello/src/main.rs
+++ bevy-hello/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
   App::new()
     .add_plugins(DefaultPlugins)
     .add_systems(Startup, setup)
+    .add_systems(Update, Dragon::movement)
     .run();
 }
 
