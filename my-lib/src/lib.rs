96fe4413de21044beb16194452dcc7df75f84bf7
David Wallace Croft <david@CroftSoft.com>
2025-12-04 19:28:55 -0600

    lib random

---
+++ my-lib/src/lib.rs
@@ -0,0 +1,4 @@
+pub use rand;
+pub use random::*;
+
+mod random;
---
+++ my-lib/src/random/mod.rs
@@ -0,0 +1,63 @@
+use ::rand::SeedableRng;
+use ::rand::prelude::*;
+use ::std::ops::Range;
+
+pub struct RandomNumberGenerator {
+  rng: StdRng,
+}
+
+impl RandomNumberGenerator {
+  pub fn range(
+    &mut self,
+    range: Range<u32>,
+  ) -> u32 {
+    self.rng.random_range(range)
+  }
+
+  pub fn seeded(seed: u64) -> Self {
+    Self {
+      rng: StdRng::seed_from_u64(seed),
+    }
+  }
+}
+
+impl Default for RandomNumberGenerator {
+  fn default() -> Self {
+    Self {
+      rng: StdRng::from_os_rng(),
+    }
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test_range_bounds() {
+    let mut rng: RandomNumberGenerator = Default::default();
+
+    for _ in 0..1_000 {
+      let n: u32 = rng.range(1..10);
+
+      assert!(n >= 1);
+
+      assert!(n < 10);
+    }
+  }
+
+  #[test]
+  fn test_reproducibility() {
+    let mut rng = (
+      RandomNumberGenerator::seeded(1),
+      RandomNumberGenerator::seeded(1),
+    );
+
+    (0..1_000).for_each(|_| {
+      assert_eq!(
+        rng.0.range(u32::MIN..u32::MAX),
+        rng.1.range(u32::MIN..u32::MAX),
+      );
+    })
+  }
+}
