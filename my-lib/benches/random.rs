e5c259b030dc779652a55987ade28a3eb26f176e
David Wallace Croft <david@CroftSoft.com>
2025-12-09 21:24:08 -0600

    criterion

---
+++ my-lib/benches/random.rs
@@ -0,0 +1,14 @@
+use ::criterion::{Criterion, criterion_group, criterion_main};
+use ::my_lib::*;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+  c.bench_function("random", |b| {
+    let mut rng: RandomNumberGenerator = Default::default();
+
+    b.iter(|| rng.range(1_f32..10_000_000_f32))
+  });
+}
+
+criterion_group!(benches, criterion_benchmark);
+
+criterion_main!(benches);
