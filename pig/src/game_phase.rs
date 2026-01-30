72b72f933e810bec313f880f0c9fb71036c15c0e
David Wallace Croft <david@CroftSoft.com>
2026-01-29 21:09:15 -0600

    game phase variants

--- pig/src/game_phase.rs
+++ pig/src/game_phase.rs
@@ -1,8 +1,13 @@
 use ::bevy::prelude::*;
 
+#[expect(dead_code)]
 #[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq, States)]
 pub enum GamePhase {
   Cpu,
+  End,
+  GameOver,
   #[default]
   Player,
+  MainMenu,
+  Start,
 }
